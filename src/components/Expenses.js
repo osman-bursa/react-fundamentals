fb4509d983655dc14921ce96a72c73947f19a257
osman-bursa <osmanbursa1996@gmail.com>
2022-09-27 21:55:09 +0300

    Card.js Component and props.children concept

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,3 +1,4 @@
+import Card from './Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
@@ -5,7 +6,7 @@ function Expenses(props) {
   const expenses = props.expenseList;
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
 
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
@@ -13,7 +14,7 @@ function Expenses(props) {
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
 
       <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
-    </div>
+    </Card>
   );
 }
 
