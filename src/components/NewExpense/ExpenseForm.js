11c3eb3cc6a8c86a55c3e7213ab4b62495991bf8
osman-bursa <osmanbursa1996@gmail.com>
2022-10-04 15:02:18 +0300

    Passing data upwards

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
@@ -61,6 +61,7 @@ const ExpenseForm = () => {
             date: new Date(enteredDate),
         };
 
+        props.onSaveExpenseData(expenseData);
         setEnteredTitle(''); // back to initial state after submittion.
         setEnteredAmount('');
         setEnteredDate('');
@@ -68,22 +69,22 @@ const ExpenseForm = () => {
 
     return (
         <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
-                <div className="new-expense__control">
+                <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
+                    <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler} />
                 </div>
-                <div className="new-expense__control">
+                <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
+                    <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
-            <div className="new-expense_actions">
-                <button type="submit">Add Expense</button>
+            <div className='new-expense_actions'>
+                <button type='submit'>Add Expense</button>
             </div>
         </form>
     );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,12 +2,23 @@ import react, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
-  return (
-    <div className="new-expense">
-      <ExpenseForm />
-    </div>
-  );
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        // taking the expenseData sent from ExpenseForm component to 'up' here
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString(), // ..adding id to it
+        };
+
+        // sending edited data to higher up in the component chain (to App.js)
+        props.onAddExpense(expenseData);
+    };
+
+    return (
+        <div className='new-expense'>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        </div>
+    );
 };
 
 export default NewExpense;
